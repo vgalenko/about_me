b42f76ac3f3aca4ef8232fce5db805b4add1c2f7
vgalenko <vitaliygalenko@gmail.com>
2017-05-09 18:00:19 -0700

    completed the about me site

---
+++ app.js
@@ -0,0 +1,48 @@
+
+var user = prompt('What is your name?');
+console.log('user: ' + user);
+
+  alert('welcome to my page ' + user);
+
+var userAnswer = prompt('Do you want to continue playing?').toLowerCase();
+console.log('userAnswer: ' + userAnswer);
+
+if (userAnswer === 'yes') {
+    alert('Nice! Lets keep going!')
+}
+
+else {
+    alert('Well... we gotta keep going anyway because this is my assignment')
+}
+
+var myAge = prompt('Guess what my age is ' + user);
+console.log('age: ' + myAge);
+
+if (myAge === '28') {
+  alert('NO WAY! How did you know?')
+}
+
+else {
+  alert ('you\'re close but not quite there yet, I am actually 28');
+}
+
+var myFood = prompt('Alright ' + user + ' lets try another one, what is my favorite food?');
+console.log('food: ' + myFood);
+
+if ('pizza' || 'burger' || 'ice cream' || 'sandwiches') {
+  alert('How did you know I like ' + myFood + ' ?');
+}
+
+else {
+  alert('try again!');
+}
+
+var userAnswerTwo = prompt('Are you bored yet?').toLowerCase();
+console.log('userAnswerTwo: ' + userAnswerTwo);
+
+if (userAnswerTwo === 'yes') {
+  alert('You\'re awesome! But we are done! Now lets look at my about me page');
+}
+else {
+  alert('Ohh man I was having fun, ok bye!');
+}
---
+++ learning-journal-1.md
@@ -0,0 +1,10 @@
+
+One of the things I learned is that when console prints out numbers, it prints them out in strings.
+So that helped me a lot with my if else statements when I use operators in there.
+
+Also learned how to properly lay out HTML5 and doing it with the proper elements rather then just always using <div> for every section. Looks much better and more organized.
+
+
+Yesterday GIT was very confusing for me but Today it completely clicked and figured out how to send my files to the repo in GitHub.
+
+Figured out how to use CSS more efficiently and hopefully less "hacky" then my previous projects.
